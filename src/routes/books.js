2c45af2da037b058b5a2c04c2b59ada44340e706
LeoMo-27 <jm.madriaza@uc.cl>
2021-06-15 02:06:36 -0400

    create/get reviews

--- src/routes/books.js
+++ src/routes/books.js
@@ -6,7 +6,21 @@ const BookSerializer = new JSONAPISerializer('books', {
   keyForAttribute: 'camelCase',
 });
 
-const BookReviewSerializer = new JSONAPISerializer('')
+const ReviewSerializer = new JSONAPISerializer('reviews', {
+  attributes: ['content', 'score', 'userId', 'bookId'],
+  keyForAttribute: 'camelCase',
+})
+
+const BookReviewSerializer = new JSONAPISerializer('reviews', {
+  attributes: ['title', 'isbn', 'author', 'genre', 'userId', 'description'],
+  reviews: {
+    ref: function(book, review) {
+      return review.id;
+    },
+    attributes: ['content', 'score', 'userId', 'bookId']
+  },
+  keyForAttribute: 'camelCase',
+});
 
 const router = new KoaRouter();
 
@@ -56,11 +70,27 @@ router.patch('api.books.patch', '/:id', loadBook, async (ctx) => {
   }
 })
 
-router.get('api.books.review', '/:id/reviews', loadBook, async (ctx) =>{
+router.get('api.books.review', '/:id/reviews', loadBook, async (ctx) => {
   const { book } = ctx.state;
   const reviews = await book.getReviews();
+  const json = ReviewSerializer.serialize(reviews);
+  if (json['data'].length === 0) {
+    ctx.throw(404, "The book you are looking for doesn't have any reviews");
+  }
+  ctx.body = json;
+})
 
-
+router.post('api.books.review.create', '/:id/reviews', loadBook, async (ctx) => {
+  try {
+    const { book } = ctx.state;
+    const review = ctx.orm.review.build(ctx.request.body);
+    await review.save( { fields: ['content', 'score', 'userId', 'bookId'] } );
+    ctx.status = 201;
+    ctx.body = ReviewSerializer.serialize(review);
+  }
+  catch { 
+    ctx.throw(404, "FAILED");
+  }
 })
 
 module.exports = router;
